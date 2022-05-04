8b5dcbe712b90b824362e2202b031b95840c5696
galo <juanba911@gmail.com>
2022-05-04 15:09:59 -0300

    dotenv data added

--- src/steps/general.steps.ts
+++ src/steps/general.steps.ts
@@ -2,6 +2,10 @@ import { ICustomWorld } from '../support/custom-world';
 import { Given, When, Then } from '@cucumber/cucumber';
 import { expect } from '@playwright/test';
 import {navigate} from '../utils/elements';
+/* eslint-disable import/first */
+require('dotenv').config();
+
+
 
 Given('a user is in the log in page', async function (this: ICustomWorld) {
   const page = this.page!;
@@ -38,17 +42,21 @@ Then('a user is logged in to the system', async function (this: ICustomWorld) {
 
 Given('the user is logged in', async function (this: ICustomWorld) {
   const page = this.page!;
+  const email = process.env.EMAIL || '';
+  const password = process.env.PASSWORD || '';
+  
+  console.log(process.env.EMAIL)
   // await page.goto('https://stage.connectamericas.com/');
   await navigate(page, 'https://stage.connectamericas.com/dashboard')
   await page.click('#loginBtn');
   await page
     .frameLocator('#login-iframe')
     .locator('input[name="username"]')
-    .fill('juanba91@hotmail.com');
+    .type(email);
 
   // await page.frameLocator('#login-iframe').locator('input[name="password"]').fill('qwe123'); change the method 'fill' for 'type' 
 
-  await page.frameLocator('#login-iframe').locator('input[name="password"]').type('qwe123');
+  await page.frameLocator('#login-iframe').locator('input[name="password"]').type(password);
 
   // await page.frameLocator('#login-iframe').locator('input[name="password"]').press('Enter');
 
