222688c4418918cbadf45114a599eeb8c0c25218
galo <juanba911@gmail.com>
2022-05-05 13:26:31 -0300

    cleaning files

--- src/steps/general.steps.ts
+++ src/steps/general.steps.ts
@@ -1,46 +1,10 @@
 import { ICustomWorld } from '../support/custom-world';
-import { Given, When, Then } from '@cucumber/cucumber';
-import { expect } from '@playwright/test';
+import { Given} from '@cucumber/cucumber';
 import { login } from '../utils/login';
 import { goToProfile } from '../utils/elements';
 
 /* eslint-disable import/first */
 require('dotenv').config();
-
-
-
-Given('a user is in the log in page', async function (this: ICustomWorld) {
-  const page = this.page!;
-  await page.goto('https://stage.connectamericas.com/');
-  await page
-    .locator(
-      'text=Search Why ConnectAmericas? Features Purchasing Announcements Connected companie >> #loginBtn',
-    )
-    .click();
-});
-When(
-  'a user enters the correct username and correct password',
-  async function (this: ICustomWorld) {
-    const page = this.page!;
-    await page
-      .frameLocator('#login-iframe')
-      .locator('input[name="username"]')
-      .fill('juanba91@hotmail.com');
-
-    await page.frameLocator('#login-iframe').locator('input[name="password"]').fill('qwe123');
-
-    await page.frameLocator('#login-iframe').locator('input[name="password"]').press('Enter');
-  },
-);
-Then('a user is logged in to the system', async function (this: ICustomWorld) {
-  const page = this.page!;
-  await page.waitForURL('https://stage.connectamericas.com/dashboard');
-  try {
-    expect(page.url()).toContain('dash');
-  } catch (error) {
-    throw new Error(`Custom errror message\n${error}`);
-  }
-});
 
 Given('the user is logged in', async function (this: ICustomWorld) {
   const page = this.page!;
