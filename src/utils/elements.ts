b1cf1ad11ab64d26e94c0c49fd8ee65361c9067e
galo <juanba911@gmail.com>
2022-05-04 21:56:43 -0300

    clickOn func added

--- src/utils/elements.ts
+++ src/utils/elements.ts
@@ -1,5 +1,10 @@
-import { Page } from '@playwright/test';
+import { Page, Locator } from '@playwright/test';
 
 export async function navigate(page: Page, url: string) {
   await page.goto(url, { timeout: 0, waitUntil: 'networkidle' });
+}
+
+export async function clickOn(element:Locator) {
+    await element.isVisible();
+    await element.click();
 }
