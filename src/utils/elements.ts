dfce52f3f73421be874918dc87175c0958f74c73
galo <juanba911@gmail.com>
2022-05-05 11:33:05 -0300

    getElement func added

--- src/utils/elements.ts
+++ src/utils/elements.ts
@@ -1,10 +1,14 @@
 import { Page, Locator } from '@playwright/test';
 
 export async function navigate(page: Page, url: string) {
-  await page.goto(url, { timeout: 0, waitUntil: 'networkidle' });
+  await page.goto(url, { waitUntil: 'networkidle' });
 }
 
 export async function clickOn(element:Locator) {
     await element.isVisible();
     await element.click();
+}
+
+export async function getElement(page: Page, element:string) {
+    return page.locator(element);    
 }
