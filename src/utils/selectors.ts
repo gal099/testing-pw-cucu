bf8429a5991ae0d4a0b845365bebd01189954324
galo <juanba911@gmail.com>
2022-05-05 13:00:34 -0300

    selectors added

--- src/utils/selectors.ts
+++ src/utils/selectors.ts
@@ -1,4 +1,10 @@
 export const selectors: {[key: string]: any}  = {
 
   loginBtn:  '#user-menu #loginBtn',
+
+  userNameInput: 'input[name="username"]',
+
+  passwordInput: 'input[name="password"]',
+
+  submitBtn: '#fm1 .btn-submit',
 }
